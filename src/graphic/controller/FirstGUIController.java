be7539b9f3e403f400af7facb579aadb5d2347db
Xyrephon <jtuc1613@740s-pl-115.local>
2016-11-01 08:21:19 -0600

    Call to constructor for the GUI.

--- src/graphic/controller/FirstGUIController.java
+++ src/graphic/controller/FirstGUIController.java
@@ -1,8 +1,16 @@
 package graphic.controller;
 
+import graphic.view.FirstFrame;
+
 public class FirstGUIController
 {
-
+	private FirstFrame appFrame;
+	
+	public FirstGUIController()
+	{
+		appFrame = new FirstFrame(this);
+	}
+	
 	public void start()
 	{
 		
